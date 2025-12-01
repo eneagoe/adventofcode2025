8b68b4815671ba890c87dda6686c10dadea2bc51
eugen neagoe <eneagoe@gmail.com>
2025-12-02 01:12:46 +0200

    Add solution for 2nd puzzle of day 1

--- day1.rb
+++ day1.rb
@@ -2,13 +2,22 @@
 
 dial = 50
 first = 0
+second = 0
 
 File.open(ARGV[0]).each do
   dir = it[0] == 'L' ? -1 : 1
   dist = it[1..].to_i
+  carry = dial.zero? ? 1 : 0
+
+  if dir.positive?
+    second += (dist + dial) / 100
+  else
+    second += (100 - (dist * dir + dial)) / 100 - carry
+  end
 
   dial = (dial + dir * dist) % 100
   first += 1 if dial.zero?
 end
 
 puts first
+puts second
